697ad50fd966853283e4db5a2f865c4c752eff9c
Helmut Neukirchen <helmut@hi.is>
2024-02-29 19:01:19 +0000

    Initial commit

---
+++ src/test/java/is/hi/hbv202g/assignment8/MainTest.java
@@ -0,0 +1,17 @@
+package is.hi.hbv202g.assignment8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest
+{
+    @Test
+    public void shouldBePossibleToInstantiateLibrarySystem()
+    {
+        new LibrarySystem();
+    }
+}
